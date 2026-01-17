3e324fc1efa4d1767234bf25b7c6995ce53be227
orignal <i2porignal@yandex.ru>
2026-01-17 08:46:57 -0500

    added reseed.sahil.world and i2p.diyarciftci.xyz reseeds

--- libi2pd/Config.cpp
+++ libi2pd/Config.cpp
@@ -261,7 +261,9 @@ namespace config {
 				"https://reseed-pl.i2pd.xyz/,"
 				"https://www2.mk16.de/,"
 			    "https://i2p.novg.net/,"
-            	"https://reseed.stormycloud.org/"
+            	"https://reseed.stormycloud.org,"
+            	"https://reseed.sahil.world/,"
+            	"https://i2p.diyarciftci.xyz/"
 			),                                                            "Reseed URLs, separated by comma")
 			("reseed.yggurls", value<std::string>()->default_value(
 				"http://[324:71e:281a:9ed3::ace]:7070/,"
