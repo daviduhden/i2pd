bb30a42c2dbe49c43ec80c826b879af8496b12a1
orignal <i2porignal@yandex.ru>
2026-02-05 18:40:21 -0500

    increase min tunnel build messages interval. don't count if from the same millisecond

--- libi2pd/I2NPProtocol.cpp
+++ libi2pd/I2NPProtocol.cpp
@@ -574,7 +574,8 @@ namespace i2p
 						if (m_NumThrottledTunnelBuildMessages < MAX_NUM_THROTTLED_TUNNEL_BUILD_MESSAGES)
 						{
 							// let TBM go through
-							m_NumThrottledTunnelBuildMessages++;
+							if (ts > m_LastTunnelBuildMessageTimestamp + 1) // don't count if from the same millisecond
+								m_NumThrottledTunnelBuildMessages++;
 							HandleI2NPMessage (msg);
 						}
 						else // drop TBM
