7b07304092b026b60578a9a6e337a1914c08e229
Sergey Fedorov <vital.had@gmail.com>
2026-03-26 05:41:19 +0800

    FS.h: add missing header

--- libi2pd/FS.h
+++ libi2pd/FS.h
@@ -14,6 +14,7 @@
 #include <iostream>
 #include <sstream>
 #include <functional>
+#include <inttypes.h>
 
 #ifndef STD_FILESYSTEM
 #	if (_WIN32 && __GNUG__) // MinGW GCC somehow incorrectly converts paths
