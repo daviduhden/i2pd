6be73912c75e849e0bc8269c0e7b7b423f351459
orignal <i2porignal@yandex.ru>
2026-03-31 18:19:24 -0400

    generate tags faster

--- libi2pd/ECIESX25519AEADRatchetSession.cpp
+++ libi2pd/ECIESX25519AEADRatchetSession.cpp
@@ -944,7 +944,7 @@ namespace garlic
 		else
 		{
 			moreTags = (receiveTagset->GetTagSetID () > 0) ? ECIESX25519_MAX_NUM_GENERATED_TAGS : // for non first tagset
-				(ECIESX25519_MIN_NUM_GENERATED_TAGS + (index >> 1)); // N/2
+				(ECIESX25519_MIN_NUM_GENERATED_TAGS + 2*index); // 2*N
 			if (moreTags > ECIESX25519_MAX_NUM_GENERATED_TAGS) moreTags = ECIESX25519_MAX_NUM_GENERATED_TAGS;
 			moreTags -= (receiveTagset->GetNextIndex () - index);
 			index -= ECIESX25519_MAX_NUM_GENERATED_TAGS; // trim behind
